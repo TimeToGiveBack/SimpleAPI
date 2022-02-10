a89ae36dccb7889403d1dfbf3858635b893941b7
Dave Shepherd <Dave.Shepherd.Home@gmail.com>
2022-02-10 13:27:08 +0200

    Fixed tests

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Dave Shepherd aa";
+            return "Dave Shepherd";
         }
 
         // POST api/values
