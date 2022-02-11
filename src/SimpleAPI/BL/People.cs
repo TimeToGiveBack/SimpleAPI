def95e9233e21938c29c1a9698f2abcf87333cb4
Dave Shepherd <Dave.Shepherd.Home@gmail.com>
2022-02-11 08:24:39 +0200

    Added mocked listing for result

---
+++ src/SimpleAPI/BL/People.cs
@@ -0,0 +1,23 @@
+using System;
+using SimpleAPI;
+using System.Collections.Generic;
+
+namespace SimpleAPI.People
+{
+    public class People
+    {
+        
+        public List<Person> GetMockList()
+        {
+            var people = new List<Person>();
+            people.Add(new Person{FirsName ="Dave", LastName = "Shepherd", Region = "SSA"});
+            people.Add(new Person{FirsName ="Keagen", LastName = "Reddy", Region = "SSA"});
+            people.Add(new Person{FirsName ="Barry", LastName = "Ahern", Region = "SSA"});
+            people.Add(new Person{FirsName ="Paul", LastName = "Steinrisser", Region = "EU"});
+            people.Add(new Person{FirsName ="Reinhard", LastName = "Schrei", Region = "EU"});
+
+            return people;
+
+        }
+    }
+}
---
+++ src/SimpleAPI/BL/Person.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SimpleAPI
+{
+    public class Person
+    {
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string Region { get; set; }
+    }
+
+}
