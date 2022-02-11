414706f0481bbfc7d66f8bd053b66d93869c84df
Dave Shepherd <Dave.Shepherd.Home@gmail.com>
2022-02-11 07:48:20 +0200

    Changed return result again

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "yes please" };
+            return new string[] { "dotnet", "yes please work" };
         }
 
         // GET api/values/5
