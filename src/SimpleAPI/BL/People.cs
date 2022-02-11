c006d004a54747e11c1d553c231d251a94c49806
Dave Shepherd <Dave.Shepherd.Home@gmail.com>
2022-02-11 08:41:58 +0200

    Added more mock items

--- src/SimpleAPI/BL/People.cs
+++ src/SimpleAPI/BL/People.cs
@@ -10,11 +10,12 @@ namespace SimpleAPI
         public List<Person> GetMockList()
         {
             var people = new List<Person>();
-            people.Add(new Person{FirsName ="Dave", LastName = "Shepherd", Region = "SSA"});
-            people.Add(new Person{FirsName ="Keagen", LastName = "Reddy", Region = "SSA"});
-            people.Add(new Person{FirsName ="Barry", LastName = "Ahern", Region = "SSA"});
+            people.Add(new Person{FirsName ="Keagen", LastName = "Reddy", Region = "SA"});
+            people.Add(new Person{FirsName ="Barry", LastName = "Ahern", Region = "SA"});
+            people.Add(new Person{FirsName ="Ivan", LastName = "van den Bergh", Region = "SA"});
             people.Add(new Person{FirsName ="Paul", LastName = "Steinrisser", Region = "EU"});
             people.Add(new Person{FirsName ="Reinhard", LastName = "Schrei", Region = "EU"});
+            //people.Add(new Person{FirsName ="Dave", LastName = "Shepherd", Region = "SA"});
 
             return people;
 
